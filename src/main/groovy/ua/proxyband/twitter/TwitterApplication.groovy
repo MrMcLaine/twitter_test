b324475732564d1ca2f5e0c41974ffbfd4ebd5d7
MrMcLaine <Lypovskyi@gmail.com>
2023-07-26 11:10:27 +0300

    init commit

---
+++ src/main/groovy/ua/proxyband/twitter/TwitterApplication.groovy
@@ -0,0 +1,13 @@
+package ua.proxyband.twitter
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TwitterApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TwitterApplication, args)
+	}
+
+}
---
+++ src/test/groovy/ua/proxyband/twitter/TwitterApplicationTests.groovy
@@ -0,0 +1,13 @@
+package ua.proxyband.twitter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
